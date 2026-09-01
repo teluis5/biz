0d5f7e7deeb7de5ef3f43005c243aefc4e75c058
teluis5 <teluis5@example.com>
2026-09-01 09:22:43 +0900

    UI: Revert site title back to telubiz

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'GitHubから';
+export const SITE_TITLE = 'telubiz | ビジネスパーソン向け実践スキルブログ';
 export const SITE_DESCRIPTION = 'コンサルティングファームで働くITエンジニアが、日々の業務で得た知見やビジネスに必要なスキルを発信します。';
